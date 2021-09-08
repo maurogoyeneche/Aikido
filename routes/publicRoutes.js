d3c5509576031e096390e651dd04da280d569d8c
maurogoyeneche <goyenechemauro@gmail.com>
2021-09-08 13:29:29 -0300

    publicRoutes.js

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -2,6 +2,13 @@ const express = require("express");
 const publicRouter = express.Router();
 
 // Rutas del Públicas:
-// ...
+publicRouter.get("/", pageController.showHome);
+publicRouter.get("/article/:slug", pageController.showArticle);
+
+publicRouter.get("/login", pageController.showLogin);
+publicRouter.get("/register", pageController.showRegister);
+
+publicRouter.post("/login", userController.login);
+publicRouter.post("/register", userController.store);
 
 module.exports = publicRouter;
