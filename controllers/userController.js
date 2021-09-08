632bc2bde4289d3ca92c3a7822c463b29b100716
FSormani <contact.fsormani@gmail.com>
2021-09-08 14:27:55 -0300

    adminController store

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,26 @@
 const { User } = require("../models");
 
-// Handlers para User...
-// ...
+const show = (req, res) => {
+  res.render("userRegister");
+};
+
+const store = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.render("home");
+  } catch (error) {
+    console.log(error);
+    res.redirect("back");
+  }
+};
+
+const update = (req, res) => {};
+
+const destroy = (req, res) => {};
+
+module.exports = {
+  show,
+  store,
+  update,
+  destroy,
+};
