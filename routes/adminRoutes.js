043be14c7bd283ee0c49f940189764f5f468fc3f
FSormani <contact.fsormani@gmail.com>
2021-09-08 13:50:37 -0300

    adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,5 +3,10 @@ const adminRouter = express.Router();
 
 // Rutas del Admin:
 // ...
+adminRouter.get("/", adminController.show);
+/* adminRouter.get("/user", adminController.show); */
+adminRouter.post("/user", adminController.store);
+adminRouter.patch("/user", adminController.update);
+adminRouter.delete("/user", adminController.destroy);
 
 module.exports = adminRouter;
