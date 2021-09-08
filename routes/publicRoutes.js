933f34b5bdc682112750b18de5933e916b3c0acf
FSormani <contact.fsormani@gmail.com>
2021-09-08 14:57:42 -0300

    update

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -4,12 +4,12 @@ const pageController = require("../controllers/pagesController");
 
 // Rutas del Públicas:
 publicRouter.get("/", pageController.showHome);
-publicRouter.get("/article/:slug", pageController.showArticle);
+/* publicRouter.get("/article/:slug", pageController.showArticle);
 
 publicRouter.get("/login", pageController.showLogin);
 publicRouter.get("/register", pageController.showRegister);
 
 publicRouter.post("/login", userController.login);
-publicRouter.post("/register", userController.store);
+publicRouter.post("/register", userController.store); */
 
 module.exports = publicRouter;
