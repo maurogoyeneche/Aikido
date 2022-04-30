da2423f621e1b9dd7696b7b208bca7112b4c703c
Mauro Goyeneche <maurogoyeneche@gmail.com>
2022-04-29 23:43:09 -0300

    delete Holi

--- aikido-frontend/src/components/Hero.jsx
+++ aikido-frontend/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/styleHero.css";
 
 function Hero() {
-  return <div className="hero"> Holi </div>;
+  return <div className="hero"> </div>;
 }
 
 export default Hero;
